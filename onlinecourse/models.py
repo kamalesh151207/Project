8d97feed1f36ea94dfc69de302f9f203bc4e0e59
kamalesh151207 <kamalesh151207@gmail.com>
2026-05-28 14:34:37 +0530

    Added models for online course

---
+++ onlinecourse/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Submission(models.Model):
+    student_name = models.CharField(max_length=100)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.student_name
