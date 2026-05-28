04dae0c3a00a247018cbc60a39859ca4bcfe39cb
kamalesh151207 <kamalesh151207@gmail.com>
2026-05-28 14:48:01 +0530

    Updated admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Question, Choice, Submission
 
-# Register your models here.
+# Choice Inline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+# Question Admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('question_text',)
+
+# Lesson Admin
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+
+# Register models
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
